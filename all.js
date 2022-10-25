293284de95a069e57e7fe81d6b36bd6850e3f618
vivi0814 <vivian91949@gmail.com>
2022-10-26 01:15:23 +0800

    1026

--- all.js
+++ all.js
@@ -22,3 +22,27 @@ window.addEventListener("scroll", function(){
         };
     }
 });
+
+// =========================數量加減=============================
+window.addEventListener("load", function(){
+    //-------------------
+    let btnPluses = document.getElementsByClassName("btnPlus");
+    let btnMinuses = document.getElementsByClassName("btnMinus");
+    let qtys = document.getElementsByClassName("qty");
+
+    for(let i=0; i<btnPluses.length; i++){
+        //------註冊 + 按鈕
+        btnPluses[i].onclick = function(e){
+            //qtys[i].value = parseInt(qtys[i].value) + 1; //+
+            let qtyBox = e.target.previousElementSibling;
+            qtyBox.value = parseInt(qtyBox.value)+1;
+        }
+
+        //------註冊 - 按鈕
+        btnMinuses[i].onclick = function(){
+            if(qtys[i].value > 0){
+                qtys[i].value = qtys[i].value - 1;//-
+            }
+        }
+    }
+})
